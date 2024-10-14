f23dc4c8feeb8a603f58acdaae0d053c67c73be8
Ziwei Luo <71720543+echovv@users.noreply.github.com>
2024-10-14 12:47:37 +1100

    Update exon_overlap.R

--- R/exon_overlap.R
+++ R/exon_overlap.R
@@ -1,19 +1,30 @@
 exon_overlap <- function(gene_A_exons, gene_B_exons){
-
   if(dim(gene_A_exons)[1] == 0 | dim(gene_B_exons)[1] == 0){
+exon_overlap <- function(gene_A_exons, gene_B_exons) {
+  if (dim(gene_A_exons)[1] == 0 | dim(gene_B_exons)[1] == 0) {
     return (FALSE)
   }
 
   for(row_exonA in 1:nrow(gene_A_exons)){
     for(row_exonB in 1:nrow(gene_B_exons)){
+  for (row_exonA in 1:nrow(gene_A_exons)) {
+    for (row_exonB in 1:nrow(gene_B_exons)) {
+      if (gene_A_exons[row_exonA, 2] >= gene_A_exons[row_exonA, 1] &&
+          gene_B_exons[row_exonB, 2] >= gene_B_exons[row_exonB, 1]) {
 
       x = seq(from = gene_A_exons[row_exonA,1], to = gene_A_exons[row_exonA,2]-1, by = 1)
       y = seq(from = gene_B_exons[row_exonB,1], to = gene_B_exons[row_exonB,2]-1, by = 1)
+        x = seq(from = gene_A_exons[row_exonA, 1], to = gene_A_exons[row_exonA, 2] - 1, by = 1)
+        y = seq(from = gene_B_exons[row_exonB, 1], to = gene_B_exons[row_exonB, 2] - 1, by = 1)
 
       if(length(intersect(x,y))!=0){
         return (TRUE)
+        if (length(intersect(x, y)) != 0) {
+          return (TRUE)
+        }
       }
     }
   }
   return (FALSE)
+}}}}}
 }
